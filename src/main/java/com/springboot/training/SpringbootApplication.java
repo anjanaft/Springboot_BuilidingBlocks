529a31af46695134f00805c23bffc161456a5334
91703 <anjanaft13@gmail.com>
2024-05-25 11:16:48 +0530

    Second commit

--- src/main/java/com/springboot/training/SpringbootApplication.java
+++ src/main/java/com/springboot/training/SpringbootApplication.java
@@ -2,10 +2,10 @@ package com.springboot.training;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//
+
 @SpringBootApplication
 public class SpringbootApplication {
-
+//
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootApplication.class, args);
 	}
