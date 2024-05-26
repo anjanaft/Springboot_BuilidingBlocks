4fc5796e09935d1d2d1a44ca084094ebb9a83e41
91703 <anjanaft13@gmail.com>
2024-05-26 16:48:24 +0530

    Exceptions

--- src/main/java/com/springboot/training/Controller/UserController.java
+++ src/main/java/com/springboot/training/Controller/UserController.java
@@ -1,9 +1,12 @@
 package com.springboot.training.Controller;
 
+import java.net.URI;
+import java.net.http.HttpHeaders;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.web.reactive.HttpHandlerAutoConfiguration;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +17,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.springboot.training.entity.User;
 import com.springboot.training.services.UserService;
@@ -29,25 +34,52 @@ public class UserController {
 	}
 	
 	@PostMapping("/users")
-	public User createUser(@RequestBody User user) {
-		return service.createUser(user);
+	public ResponseEntity<URI> createUser(@RequestBody User user) {
+		try {
+			
+			service.createUser(user);
+			URI loc = ServletUriComponentsBuilder
+					.fromCurrentRequest()
+					.path("/{id}")
+					.buildAndExpand(user.getId())
+					.toUri();
+			return new ResponseEntity<URI>(loc,HttpStatus.CREATED);
+		}catch(Exception e) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST,e.getMessage());
+		}
+		
 	}
 	
 	@GetMapping("user/{id}")
-	public Optional<User> getUserById(@PathVariable("id") Long Id) {
-		return service.getUserById(Id);
+	public Optional<User> getUserById(@PathVariable("id") Long Id)  {
+		try {
+			return service.getUserById(Id);
+		}catch(Exception e) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND,e.getMessage());
+		}
+		
 	}
 	
 	@PutMapping("user/{id}")
 	public User updateUserById(@RequestBody User user, @PathVariable("id") Long id) {
-		return service.updateUserById(user, id);
+		try {
+			return service.updateUserById(user, id);
+		}catch (Exception e) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST,e.getMessage());
+		}
+		
 	}
 	
 	@DeleteMapping("user/{id}")
-	public ResponseEntity<?> deleteUserById(@PathVariable("id") Long id){
-		service.deleteUserById(id);
-		return new ResponseEntity<>(HttpStatus.OK);
-	}
+	public void deleteUserById(@PathVariable("id") Long id){
+		try {
+			service.deleteUserById(id);
+		}catch (Exception e) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST,e.getMessage());
+		}
+		}
+		
+		
 	@GetMapping("username/{username}")
 	public User getByUsername(@PathVariable("username") String username) {
 		return  service.getByUserName(username);
--- src/main/java/com/springboot/training/services/UserService.java
+++ src/main/java/com/springboot/training/services/UserService.java
@@ -9,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.springboot.training.entity.User;
+import com.springboot.training.exception.UserExistsException;
+import com.springboot.training.exception.UserNotFoundException;
 import com.springboot.training.repositories.UserRepository;
 
 @Service
@@ -21,19 +23,29 @@ public class UserService {
 		return userRepo.findAll();
 	}
 	
-	public User createUser(User user) {
+	public User createUser(User user) throws UserExistsException {
+		User existingUser = userRepo.getByUsername(user.getUsername());
+		if(existingUser != null) throw new UserExistsException("User already exists");
 		return userRepo.save(user);
 	}
 	
-	public Optional<User> getUserById(Long id) {
-		return userRepo.findById(id);
+	public Optional<User> getUserById(Long id) throws UserNotFoundException {
+		Optional<User> user = userRepo.findById(id);
+		if(!user.isPresent()) { throw new UserNotFoundException("User doesn't exists");}
+		return user;
 	}
 	
-	public User updateUserById(User user , Long id) {
+	public User updateUserById(User user , Long id) throws UserNotFoundException {
+		Optional<User> optUser = userRepo.findById(id);
+		if(!optUser.isPresent()) throw new UserNotFoundException("User doesn't exist");
+		
 		user.setId(id);
 		return userRepo.save(user);
 	}
-	public void deleteUserById(Long id) {
+	public void deleteUserById(Long id) throws UserNotFoundException {
+		Optional<User> optUser = userRepo.findById(id);
+		if(!optUser.isPresent()) throw new UserNotFoundException("User doesn't exist");
+		
 		userRepo.deleteById(id);
 		 
 	}
